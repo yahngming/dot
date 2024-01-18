65072ff613ebab74a87cf01d0bcbc2413d680847
Yang Ming <yahngming@gmail.com>
2024-01-18 22:53:57 +0800

    Update archpac.sh

--- archpac.sh
+++ archpac.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo pacman -S --needed base-devel btop feh firefox git neofetch ranger zsh
+sudo pacman -S --needed base-devel btop feh firefox git neofetch nnn zsh
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
