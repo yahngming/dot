913c4836adafbf323e8bed22f2324e8b27825530
Yang Ming <yahngming@gmail.com>
2024-01-20 01:53:26 +0800

    Update archpac.sh

--- archpac.sh
+++ archpac.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-sudo pacman -S --needed aria2 base-devel bc btop cmatrix dictd feh firefox git links mpv neofetch nnn noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ntp pianobar zsh
+sudo pacman -S aria2 bc btop cmatrix dictd feh links mpv neofetch nnn ntp pianobar zsh
+sudo pacman -S fcitx5-chinese-addons fcitx5-im noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-nerd-fonts-symbols
+sudo pacman -S firefox
+sudo pacman -S base-devel git
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
