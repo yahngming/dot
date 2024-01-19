1d44e40c9cc1a27bb5a5d634f1e8da17f0288889
Yang Ming <yahngming@gmail.com>
2024-01-19 17:54:13 +0800

    Update archpac.sh

--- archpac.sh
+++ archpac.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo pacman -S --needed aria2 base-devel bc btop cmatrix cmus dictd feh firefox git links mpv neofetch nnn ntp zsh
+sudo pacman -S --needed aria2 base-devel bc btop cmatrix dictd feh firefox git links mpv neofetch nnn ntp pianobar zsh
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
