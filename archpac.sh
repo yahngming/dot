787479a350f7c2139d78c0312109768e413fa4a5
Yang Ming <yahngming@gmail.com>
2024-01-21 21:28:21 +0800

    Update archpac.sh

--- archpac.sh
+++ archpac.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
-sudo pacman -S aria2 base-devel bc btop cmatrix dictd feh git links mpv neofetch nnn ntp pianobar zsh
-sudo pacman -S fcitx5-chinese-addons fcitx5-im noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-nerd-fonts-symbols
-sudo pacman -S firefox
+
+sudo pacman -S fcitx5-chinese-addons fcitx5-im feh firefox git noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-nerd-fonts-symbols python-pywal vim xorg-xrandr
+
+mkdir ~/.local/src && cd ~/.local/src
+git clone https://git.suckless.org/dwm
+git clone https://git.suckless.org/dmenu
+git clone https://git.suckless.org/st
+cd ~/.local/src/dwm && sudo make clean install
+cd ~/.local/src/dmenu && sudo make clean install
+cd ~/.local/src/st && sudo make clean install
+
+cp /etc/X11/xinit/xinitrc ~/.xinitrc
+vim ~/.xinitrc # xrandr,wal,exec
+vim ~/.bashrc # (cat ~/.cache/wal/sequences &)
+vim ~/.bash_profile # startx at login
+
+sudo pacman -S aria2 base-devel bc btop cmatrix dictd feh git links mpv neofetch nnn pianobar
+
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
