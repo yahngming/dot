ef0d5b4500c8e3c6b0c7291f58fa502037670770
Yang Ming <yahngming@gmail.com>
2024-01-29 03:07:27 +0800

    Create config.sh

---
+++ .local/bin/config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl https://raw.githubusercontent.com/yahngming/linux/main/.config/bash/bash_profile -o ~/.config/bash/bash_profile
+curl https://raw.githubusercontent.com/yahngming/linux/main/.config/vim/vimrc -o ~/.config/vim/vimrc
+curl https://raw.githubusercontent.com/yahngming/linux/main/.config/xorg/xinitrc -o ~/.config/xorg/xinitrc
