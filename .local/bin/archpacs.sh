457ca89ab2e35d79ffd400ab36a808521605dc47
Yang Ming <yahngming@gmail.com>
2024-01-28 18:26:29 +0800

    Update archpacs.sh

--- .local/bin/archpacs.sh
+++ .local/bin/archpacs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo pacman -S base-devel btop chafa cmatrix fcitx5-chinese-addons fcitx5-im firefox git lf maim man-db mpv neofetch noto-fonts noto-fonts-cjk noto-fonts-emoji nsxiv translate-shell trash-cli ttf-nerd-fonts-symbols pianobar python-pywal vim xdotool xwallpaper xorg-xrandr zathura zathura-pdf-mupdf
+sudo pacman -S base-devel btop chafa cmatrix fcitx5-chinese-addons fcitx5-im firefox git lf maim man-db mpv neofetch neomutt noto-fonts noto-fonts-cjk noto-fonts-emoji nsxiv translate-shell trash-cli ttf-nerd-fonts-symbols pianobar python-pywal vim xdotool xwallpaper xorg-xrandr zathura zathura-pdf-mupdf
