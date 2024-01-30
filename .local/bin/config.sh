2ec38a638554be5bc032feffe8fab2f192b331e6
Yang Ming <yahngming@gmail.com>
2024-01-30 19:05:01 +0800

    Update config.sh

--- .local/bin/config.sh
+++ .local/bin/config.sh
@@ -3,3 +3,4 @@
 curl https://raw.githubusercontent.com/yahngming/linux/main/.config/bash/bash_profile -o ~/.config/bash/bash_profile
 curl https://raw.githubusercontent.com/yahngming/linux/main/.config/vim/vimrc -o ~/.config/vim/vimrc
 curl https://raw.githubusercontent.com/yahngming/linux/main/.config/xorg/xinitrc -o ~/.config/xorg/xinitrc
+curl https://raw.githubusercontent.com/yahngming/linux/main/.config/newsboat/urls -o ~/.config/newsboat/urls
