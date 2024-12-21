385b06c94dea138947d7e676bd07cb2d27021ef1
Yang Ming <yahngming@gmail.com>
2024-12-21 20:11:15 +0800

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -3,6 +3,7 @@
 arch() {
 	sudo pacman -Syu aerc atool base-devel bat bc btop clash cmus dante eza fastfetch fcitx5-chinese-addons fcitx5-im fd ffmpegthumbnailer firefox fontforge fzf git git-delta glow gvim jq less lf libreoffice-fresh lolcat maim man-db mpv noto-fonts noto-fonts-cjk noto-fonts-emoji nsxiv openssh pass perl-image-exiftool polkit poppler python-pywal steam translate-shell trash-cli ttf-nerd-fonts-symbols ueberzugpp w3m xcape xclip xcompmgr xdo xorg-xrandr xssstate xwallpaper zathura-pdf-poppler zoxide
  	mkdir -p ~/.config/clash
+	mkdir -p ~/.config/firefox
 	mkdir -p ~/.local/src
 	mkdir -p ~/Pictures/wallpapers
 	cd ~/.local/src && git clone https://github.com/yahngming/dwm && cd ~/.local/src/dwm && sudo make clean install
@@ -13,7 +14,10 @@ arch() {
 	cd ~/.local/src && git clone https://aur.archlinux.org/yay.git && cd ~/.local/src/yay && makepkg -si
 	yay -S ctpv
  	yay -S newsraft
-	curl -Lo  ~/Pictures/wallpapers/arch.png https://dt.iki.fi/stuff/forums/arch/ARCH_bubblessvg_v2_5120px.png
+	curl -Lo ~/.config/firefox/prefsCleaner.sh https://cdn.jsdelivr.net/gh/arkenfox/user.js/prefsCleaner.sh
+	curl -Lo ~/.config/firefox/updater.sh https://cdn.jsdelivr.net/gh/arkenfox/user.js/updater.sh
+	curl -Lo ~/.config/firefox/user.js https://cdn.jsdelivr.net/gh/arkenfox/user.js/user.js
+	curl -Lo ~/Pictures/wallpapers/arch.png https://dt.iki.fi/stuff/forums/arch/ARCH_bubblessvg_v2_5120px.png
 	gsettings set org.gnome.system.proxy.ftp host '127.0.0.1'
 	gsettings set org.gnome.system.proxy.ftp port 7890
 	gsettings set org.gnome.system.proxy.http host '127.0.0.1'
