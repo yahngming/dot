039ff54a15814b7c842706dc4f6376599ab32da3
Yang Ming <yahngming@gmail.com>
2025-01-01 00:21:20 +0800

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -16,8 +16,9 @@ arch() {
 	mkdir -p ~/.config/firefox && cd ~/.config/firefox && curl -O https://cdn.jsdelivr.net/gh/arkenfox/user.js/prefsCleaner.sh && curl -O https://cdn.jsdelivr.net/gh/arkenfox/user.js/updater.sh && chmod +x *.sh && ./updater.sh
 	mkdir -p ~/Pictures/wallpapers && cd ~/Pictures/wallpapers && curl -O https://dt.iki.fi/stuff/forums/arch/ARCH_bubblessvg_v2_5120px.png
  	sudo cp -r ~/.config/getty\@.service.d/ /etc/systemd/system/
-  	sudo systemctl start bluetooth.service
+   	sudo systemctl enable avahi-daemon.service
    	sudo systemctl enable bluetooth.service
+   	sudo systemctl enable smb.service
 	gsettings set org.gnome.system.proxy.ftp host '127.0.0.1'
 	gsettings set org.gnome.system.proxy.ftp port 7890
 	gsettings set org.gnome.system.proxy.http host '127.0.0.1'
