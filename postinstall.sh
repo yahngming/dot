1c4117e287c07dfe7ba12a6fbd0dc716191031fd
Yang Ming <yahngming@gmail.com>
2025-01-03 04:21:11 +0800

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 arch() {
-	sudo pacman -Syu 7zip aerc atool base-devel bat bc bluez bluez-utils btop clash cmus cups dante eza fastfetch fcitx5-chinese-addons fcitx5-im fd ffmpegthumbnailer firefox fontforge fzf git git-delta glow gvim jq less lf libreoffice-fresh lolcat maim man-db mpv noto-fonts noto-fonts-cjk noto-fonts-emoji nsxiv openssh pass perl-image-exiftool polkit poppler python-pywal samba steam translate-shell trash-cli ttf-nerd-fonts-symbols ueberzugpp unzip w3m xcape xclip xcompmgr xdo xorg-xrandr xssstate xwallpaper zathura-pdf-poppler zoxide
+	sudo pacman -Syu 7zip aerc atool base-devel bat bc bluez bluez-utils btop clash cmus cups dante eza fastfetch fcitx5-chinese-addons fcitx5-im fd ffmpegthumbnailer firefox fontforge fzf git git-delta glow gvim jq less lf libreoffice-fresh lolcat maim man-db mpv noto-fonts noto-fonts-cjk noto-fonts-emoji nsxiv openssh pass perl-image-exiftool polkit poppler python-pywal samba steam translate-shell trash-cli ttf-nerd-fonts-symbols udiskie ueberzugpp unzip w3m xcape xclip xcompmgr xdo xorg-xrandr xssstate xwallpaper zathura-pdf-poppler zoxide
 	mkdir -p ~/.local/src
  	cd ~/.local/src && git clone https://github.com/yahngming/dot && cd dot && bash .local/bin/dot
 	cd ~/.local/src && git clone https://github.com/yahngming/dwm && cd dwm && sudo make clean install
