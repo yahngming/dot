9bc43cccd40c2929959ca501306d7ff632c6607e
Yang Ming <yahngming@gmail.com>
2025-01-07 21:19:08 +0800

    Update user-overrides.js

--- .config/firefox/user-overrides.js
+++ .config/firefox/user-overrides.js
@@ -1,3 +1,3 @@
 user_pref("privacy.resistFingerprinting.letterboxing", false);
-user_pref("ui.caretBlinkTime", -1);
-user_pref("ui.caretWidth", 16);
+user_pref("ui.caretBlinkTime", 0);
+user_pref("ui.caretWidth", 4);
