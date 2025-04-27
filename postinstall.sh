740744ef4d3200978982163a6c5cc0c66318dc47
Yang Ming <yahngming@gmail.com>
2025-04-27 15:22:28 +0800

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -22,15 +22,7 @@ arch() {
    	sudo systemctl enable smb.service
  	sudo cp -r ~/.config/getty\@.service.d/ /etc/systemd/system/
 	sudo sed -i 's/LOGIN_TIMEOUT.*/LOGIN_TIMEOUT 0/' /etc/login.defs
-	gsettings set org.gnome.system.proxy.ftp host '127.0.0.1'
-	gsettings set org.gnome.system.proxy.ftp port 7890
-	gsettings set org.gnome.system.proxy.http host '127.0.0.1'
-	gsettings set org.gnome.system.proxy.http port 7890
-	gsettings set org.gnome.system.proxy.https host '127.0.0.1'
-	gsettings set org.gnome.system.proxy.https port 7890
-	gsettings set org.gnome.system.proxy.socks host '127.0.0.1'
-	gsettings set org.gnome.system.proxy.socks port 7891
-	gsettings set org.gnome.system.proxy mode 'none'
+	sudo gpasswd -a $USER uucp
 }
 
 chromeos() {
