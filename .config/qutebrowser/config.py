00f246be88ca19b2d9be5a5045d8ba1accc6d33a
Yang Ming <yahngming@gmail.com>
2025-10-16 16:34:52 +0800

    Update config.py

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -2,15 +2,10 @@ import json
 import os
 
 try:
-    bg, fg, cursor = '#000000', '#FFFFFF', '#FFFFFF'
     for i in range(16):
         globals()[f'c{i}'] = '#888888'
     with open(os.path.expanduser('~/.cache/wal/colors.json'), 'r') as f:
         data = json.load(f)
-    special = data.get('special', {})
-    bg = special.get('background', bg)
-    fg = special.get('foreground', fg)
-    cursor = special.get('cursor', cursor)
     colors = data.get('colors', {})
     for i in range(16):
         globals()[f'c{i}'] = colors.get(f'color{i}', globals().get(f'c{i}'))
@@ -20,33 +15,33 @@ except (FileNotFoundError, Exception):
 c.fonts.default_family = 'monospace'
 
 # Status bar colors
-c.colors.statusbar.normal.bg = bg
-c.colors.statusbar.normal.fg = fg
+c.colors.statusbar.normal.bg = c0
+c.colors.statusbar.normal.fg = c7
 c.colors.statusbar.insert.bg = c2
 c.colors.statusbar.command.fg = c3
 c.colors.statusbar.passthrough.bg = c4
-c.colors.statusbar.passthrough.fg = fg
+c.colors.statusbar.passthrough.fg = c7
 # Tab colors
-c.colors.tabs.even.bg = bg
-c.colors.tabs.even.fg = fg
-c.colors.tabs.odd.bg = bg
-c.colors.tabs.odd.fg = fg
+c.colors.tabs.even.bg = c0
+c.colors.tabs.even.fg = c4
+c.colors.tabs.odd.bg = c0
+c.colors.tabs.odd.fg = c4
 c.colors.tabs.selected.even.bg = c3
 c.colors.tabs.selected.even.fg = c7
 c.colors.tabs.selected.odd.bg = c3
 c.colors.tabs.selected.odd.fg = c7
 # Hint colors
 c.colors.hints.bg = c3
-c.colors.hints.fg = bg
+c.colors.hints.fg = c0
 c.colors.hints.match.fg = c1
 # Completion menu colors
-c.colors.completion.odd.bg = bg
+c.colors.completion.odd.bg = c0
 c.colors.completion.even.bg = c0
-c.colors.completion.fg = fg
+c.colors.completion.fg = c7
 c.colors.completion.item.selected.bg = c4
-c.colors.completion.item.selected.fg = fg
+c.colors.completion.item.selected.fg = c7
 # Downloads bar colors
-c.colors.downloads.bar.bg = bg
+c.colors.downloads.bar.bg = c0
 c.colors.downloads.start.fg = c4
 c.colors.downloads.stop.fg = c2
 
