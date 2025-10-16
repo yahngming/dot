b05bd1f8ad0241f29bf02dc8981b864cf740f7a9
Yang Ming <yahngming@gmail.com>
2025-10-16 16:04:18 +0800

    Update config.py

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,49 +1,62 @@
-config.load_autoconfig(False)
+import json
+import os
 
-c.colors.statusbar.normal.bg = "#00000000"
-c.colors.statusbar.command.bg = "#00000000"
-c.colors.statusbar.command.fg = foreground
-c.colors.statusbar.normal.fg = color14
-c.colors.statusbar.passthrough.fg = color14
-c.colors.statusbar.url.fg = color13
-c.colors.statusbar.url.success.https.fg = color13
-c.colors.statusbar.url.hover.fg = color12
-c.colors.tabs.even.bg = "#00000000"
-c.colors.tabs.odd.bg = "#00000000"
-c.colors.tabs.bar.bg = "#00000000"
-c.colors.tabs.even.fg = color0
-c.colors.tabs.odd.fg = color0
-c.colors.tabs.selected.even.bg = foreground
-c.colors.tabs.selected.odd.bg = foreground
-c.colors.tabs.selected.even.fg = background
-c.colors.tabs.selected.odd.fg = background
-c.colors.hints.bg = background
-c.colors.hints.fg = foreground
-c.colors.completion.item.selected.match.fg = color6
-c.colors.completion.match.fg = color6
-c.colors.tabs.indicator.start = color10
-c.colors.tabs.indicator.stop = color8
-c.colors.completion.odd.bg = background
-c.colors.completion.even.bg = background
-c.colors.completion.fg = foreground
-c.colors.completion.category.bg = background
-c.colors.completion.category.fg = foreground
-c.colors.completion.item.selected.bg = background
-c.colors.completion.item.selected.fg = foreground
+WAL_CACHE_PATH = os.path.expanduser('~/.cache/wal/colors.json')
+try:
+    with open(WAL_CACHE_PATH, 'r') as f:
+        data = json.load(f)
+    if 'colors' in data and isinstance(data['colors'], dict):
+        for i in range(16):
+            key = f'color{i}'
+            if key in data['colors']:
+                globals()[f'c{i}'] = data['colors'][key]
+    if 'special' in data and isinstance(data['special'], dict):
+        if 'background' in data['special']:
+            bg = data['special']['background']
+        if 'foreground' in data['special']:
+            fg = data['special']['foreground']
+        if 'cursor' in data['special']:
+            cursor = data['special']['cursor']
 
-c.colors.messages.info.bg = background
-c.colors.messages.info.fg = foreground
-c.colors.messages.error.bg = background
-c.colors.messages.error.fg = foreground
-c.colors.downloads.error.bg = background
-c.colors.downloads.error.fg = foreground
-c.colors.downloads.bar.bg = background
-c.colors.downloads.start.bg = color10
-c.colors.downloads.start.fg = foreground
-c.colors.downloads.stop.bg = color8
-c.colors.downloads.stop.fg = foreground
-c.colors.tooltip.bg = background
-c.colors.webpage.bg = background
-c.hints.border = foreground
+# Set the font
+c.fonts.default_family = 'monospace'
+
+# Status bar colors
+c.colors.statusbar.normal.bg = bg
+c.colors.statusbar.normal.fg = fg
+c.colors.statusbar.insert.bg = c2
+c.colors.statusbar.command.fg = c3
+c.colors.statusbar.passthrough.bg = c4
+c.colors.statusbar.passthrough.fg = fg
+
+# Tab colors
+c.colors.tabs.even.bg = bg
+c.colors.tabs.even.fg = fg
+c.colors.tabs.odd.bg = bg
+c.colors.tabs.odd.fg = fg
+c.colors.tabs.selected.even.bg = c4
+c.colors.tabs.selected.even.fg = c15
+c.colors.tabs.selected.odd.bg = c4
+c.colors.tabs.selected.odd.fg = c15
+
+# Hint colors
+c.colors.hints.bg = c3
+c.colors.hints.fg = bg
+c.colors.hints.match.fg = c1
+
+# Completion menu colors
+c.colors.completion.odd.bg = bg
+c.colors.completion.even.bg = c0
+c.colors.completion.fg = fg
+c.colors.completion.selected.bg = c4
+c.colors.completion.selected.fg = fg
+
+# Downloads bar colors
+c.colors.downloads.bar.bg = bg
+c.colors.downloads.system.fg = fg
+c.colors.downloads.start.fg = c4
+c.colors.downloads.stop.fg = c2
 
 c.content.blocking.enabled = True
+
+config.load_autoconfig(False)
