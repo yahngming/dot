c6a91cabdab23eb8a7c7eb227244d733ccc18be4
Yang Ming <yahngming@gmail.com>
2025-10-16 16:29:22 +0800

    Update config.py

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -17,7 +17,6 @@ try:
 except (FileNotFoundError, Exception):
     pass
 
-# Set the font
 c.fonts.default_family = 'monospace'
 
 # Status bar colors
@@ -27,7 +26,6 @@ c.colors.statusbar.insert.bg = c2
 c.colors.statusbar.command.fg = c3
 c.colors.statusbar.passthrough.bg = c4
 c.colors.statusbar.passthrough.fg = fg
-
 # Tab colors
 c.colors.tabs.even.bg = bg
 c.colors.tabs.even.fg = fg
@@ -37,22 +35,18 @@ c.colors.tabs.selected.even.bg = c4
 c.colors.tabs.selected.even.fg = c15
 c.colors.tabs.selected.odd.bg = c4
 c.colors.tabs.selected.odd.fg = c15
-
 # Hint colors
 c.colors.hints.bg = c3
 c.colors.hints.fg = bg
 c.colors.hints.match.fg = c1
-
 # Completion menu colors
 c.colors.completion.odd.bg = bg
 c.colors.completion.even.bg = c0
 c.colors.completion.fg = fg
 c.colors.completion.item.selected.bg = c4
 c.colors.completion.item.selected.fg = fg
-
 # Downloads bar colors
 c.colors.downloads.bar.bg = bg
-c.colors.downloads.system.fg = fg
 c.colors.downloads.start.fg = c4
 c.colors.downloads.stop.fg = c2
 
