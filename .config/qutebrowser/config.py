28bd72ed1070e65173fcc3c3753888c441e74e0f
Yang Ming <yahngming@gmail.com>
2025-10-18 15:36:07 +0800

    Update config.py

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -46,10 +46,16 @@ c.content.blocking.adblock.lists = [
 	'https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances-cookies.txt',
 	'https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances-others.txt',
 	'https://github.com/uBlockOrigin/uAssets/raw/master/filters/badware.txt',
+	'https://github.com/uBlockOrigin/uAssets/raw/master/filters/experimental.txt',
 	'https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters.txt',
+	'https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-general.txt',
+	'https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-mobile.txt',
+	'https://github.com/uBlockOrigin/uAssets/raw/master/filters/lan-block.txt',
 	'https://github.com/uBlockOrigin/uAssets/raw/master/filters/privacy.txt',
 	'https://github.com/uBlockOrigin/uAssets/raw/master/filters/quick-fixes.txt',
 	'https://github.com/uBlockOrigin/uAssets/raw/master/filters/resource-abuse.txt',
+	'https://github.com/uBlockOrigin/uAssets/raw/master/filters/ubo-link-shorteners.txt',
+	'https://github.com/uBlockOrigin/uAssets/raw/master/filters/ubol-filters.txt',
 	'https://github.com/uBlockOrigin/uAssets/raw/master/filters/unbreak.txt'
 ]
 c.downloads.position = 'bottom'
