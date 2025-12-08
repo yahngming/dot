8a5b0fd1b7a34278301902fec009ef1b7605ae58
Yang Ming <yahngming@gmail.com>
2025-12-09 05:18:48 +0800

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -5,7 +5,7 @@ arch() {
 	sudo sed -i '/#\[multilib\]/,+1s/^#//' /etc/pacman.conf
 	sudo pacman -Syu --needed archlinux-keyring
 	sudo pacman -S bluez bluez-utils brightnessctl evtest intel-media-driver lib32-mesa lib32-vulkan-icd-loader lib32-vulkan-intel lib32-vulkan-radeon mesa pipewire pipewire-alsa pipewire-audio pipewire-jack udiskie vulkan-icd-loader vulkan-intel vulkan-radeon wireplumber # driver
-	sudo pacman -S 7zip android-tools atool base-devel bat bc cups dante docker docker-compose eza fastfetch fd ffmpegthumbnailer fontforge fortune fuse fzf git git-delta glow id3v2 inetutils jq less lolcat man-db mpv nsxiv openssh pass perl-image-exiftool polkit poppler python-adblock python-pywal rclone ripgrep samba translate-shell ueberzugpp unrar unzip w3m webkit2gtk-4.1 yt-dlp zathura-pdf-poppler zoxide # base
+	sudo pacman -S 7zip android-tools atool base-devel bat bc cups dante docker docker-compose eza fastfetch fd ffmpegthumbnailer fontforge fortune-mod fuse fzf git git-delta glow id3v2 inetutils jq less lolcat man-db mpv nsxiv openssh pass perl-image-exiftool polkit poppler python-adblock python-pywal rclone ripgrep samba translate-shell ueberzugpp unrar unzip w3m webkit2gtk-4.1 yt-dlp zathura-pdf-poppler zoxide # base
 	sudo pacman -S noto-fonts-cjk noto-fonts-emoji ttf-input-nerd ttf-noto-nerd ttf-terminus-nerd # font
 	sudo pacman -S aerc bluetui btop cmus impala lf wiremix # tui
 	sudo pacman -S gvim keepassxc libreoffice-fresh qutebrowser steam # gui
