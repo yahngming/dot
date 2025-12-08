d939cf6d2bf2dd8d578d78efb4edb112e8f93f4c
Yang Ming <yahngming@gmail.com>
2025-12-08 21:42:21 +0800

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -2,6 +2,7 @@
 
 arch() {
 	sudo sed -i '/#\[multilib\]/,+1s/^#//' /etc/pacman.conf
+	sudo pacman -Syy
 	sudo pacman -S bluez bluez-utils brightnessctl evtest intel-media-driver mesa pipewire-jack udiskie # driver
 	sudo pacman -S 7zip android-tools atool base-devel bat bc cups dante docker docker-compose eza fastfetch fd ffmpegthumbnailer fontforge fuse fzf git git-delta glow id3v2 inetutils jq less lolcat man-db mpv nsxiv openssh pass perl-image-exiftool polkit poppler python-adblock python-pywal rclone ripgrep samba translate-shell ueberzugpp unrar unzip w3m yt-dlp zathura-pdf-poppler zoxide # base
 	sudo pacman -S noto-fonts-cjk noto-fonts-emoji ttf-noto-nerd ttf-terminus-nerd # font
