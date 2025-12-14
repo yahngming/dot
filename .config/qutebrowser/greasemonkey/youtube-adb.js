6da44d9fecac86354e247d2d296ab63ea5a4b597
Yang Ming <yahngming@gmail.com>
2025-12-14 21:39:56 +0800

    Update youtube-adb.js

--- .config/qutebrowser/greasemonkey/youtube-adb.js
+++ .config/qutebrowser/greasemonkey/youtube-adb.js
@@ -1,11 +1,35 @@
+// ==UserScript==
+// @name         youtube-adb
+// @name:zh-CN   YouTube去广告
+// @name:zh-TW   YouTube去廣告
+// @name:zh-HK   YouTube去廣告
+// @name:zh-MO   YouTube去廣告
+// @namespace    https://github.com/iamfugui/youtube-adb
+// @version      6.21
+// @description         A script to remove YouTube ads, including static ads and video ads, without interfering with the network and ensuring safety.
+// @description:zh-CN   脚本用于移除YouTube广告，包括静态广告和视频广告。不会干扰网络，安全。
+// @description:zh-TW   腳本用於移除 YouTube 廣告，包括靜態廣告和視頻廣告。不會干擾網路，安全。
+// @description:zh-HK   腳本用於移除 YouTube 廣告，包括靜態廣告和視頻廣告。不會干擾網路，安全。
+// @description:zh-MO   腳本用於移除 YouTube 廣告，包括靜態廣告和視頻廣告。不會干擾網路，安全。
+// @match        *://*.youtube.com/*
+// @exclude      *://accounts.youtube.com/*
+// @exclude      *://www.youtube.com/live_chat_replay*
+// @exclude      *://www.youtube.com/persist_identity*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=YouTube.com
+// @grant        none
+// @license MIT
+// @downloadURL https://update.greasyfork.org/scripts/459541/YouTube%E5%8E%BB%E5%B9%BF%E5%91%8A.user.js
+// @updateURL https://update.greasyfork.org/scripts/459541/YouTube%E5%8E%BB%E5%B9%BF%E5%91%8A.meta.js
+// ==/UserScript==
+
 (function() {
     `use strict`;
 
     let video;
     //界面广告选择器
     const cssSelectorArr = [
-        `ytd-rich-section-renderer`,
-        `ytd-rich-shelf-renderer`,
+        `ytd-rich-item-renderer`,
+        `ytm-companion-ad-renderer`,
     ];
     window.dev=false;//开发使用
 
