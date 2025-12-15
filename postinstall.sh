46f7b2a453c1490dfb5883de3421f687b8c7f3c4
Yang Ming <yahngming@gmail.com>
2025-12-15 15:58:36 +0800

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -5,7 +5,7 @@ arch() {
 	sudo sed -i '/#\[multilib\]/,+1s/^#//' /etc/pacman.conf
 	sudo pacman -Syu --needed --noconfirm archlinux-keyring
 	sudo pacman -S --noconfirm bluez intel-media-driver lib32-mesa lib32-vulkan-icd-loader lib32-vulkan-intel lib32-vulkan-radeon libvdpau-va-gl libvpl mesa pipewire pipewire-alsa pipewire-jack pipewire-pulse vpl-gpu-rt vulkan-icd-loader vulkan-intel vulkan-radeon # driver
-	sudo pacman -S --noconfirm 7zip aerc atool base-devel bat bc bluetui bluez-utils brightnessctl btop cmus cups dante docker docker-compose evtest eza fastfetch fd ffmpegthumbnailer fontforge fortune-mod fuse fzf git git-delta glow id3v2 impala inetutils jq less lf lolcat man-db mpv openssh pass perl-image-exiftool polkit poppler python-adblock python-pywal rclone ripgrep samba translate-shell udiskie ueberzugpp unrar unzip w3m webkit2gtk-4.1 wiremix wireplumber yt-dlp zathura-pdf-poppler zoxide # base
+	sudo pacman -S --noconfirm 7zip aerc atool base-devel bat bc bluetui bluez-utils brightnessctl btop cmus cups dante docker docker-compose evtest eza fastfetch fd ffmpegthumbnailer fontforge fortune-mod fuse fzf git git-delta glow id3v2 impala inetutils jq lazydocker less lf lolcat man-db mpv openssh pass perl-image-exiftool polkit poppler python-adblock python-pywal rclone ripgrep samba translate-shell udiskie ueberzugpp unrar unzip w3m webkit2gtk-4.1 wiremix wireplumber yt-dlp zathura-pdf-poppler zoxide # base
 	sudo pacman -S --noconfirm noto-fonts-cjk noto-fonts-emoji ttf-liberation ttf-noto-nerd ttf-terminus-nerd # font
 	sudo pacman -S --noconfirm gamescope gvim keepassxc libreoffice-fresh qutebrowser steam # gui
 	sudo pacman -S --noconfirm fcitx5-chinese-addons fcitx5-im fcitx5-mozc # ime
