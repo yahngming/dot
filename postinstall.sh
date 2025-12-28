067c8edc7e2f1b84d2512ed1351a8b3f882b12eb
Yang Ming <yahngming@gmail.com>
2025-12-29 00:51:47 +0800

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -13,7 +13,6 @@ arch() {
 	sudo pacman -S --noconfirm fcitx5-chinese-addons fcitx5-im fcitx5-mozc # ime
 	sudo pacman -S --noconfirm cliphist foot fuzzel grim hypridle hyprland hyprlock hyprpaper kanshi mako niri qt6-wayland satty slurp swaybg waybar wl-clipboard wl-mirror wlr-randr xwayland-satellite # wayland
 	sudo pacman -S --noconfirm clipmenu dunst maim nsxiv numlockx unclutter xcape xclip xcompmgr xdg-desktop-portal-gtk xdo xfce4 xorg-server xorg-xinit xorg-xinput xorg-xrandr xscreensaver xwallpaper # xorg
-	sudo systemctl enable avahi-daemon.service
 	sudo systemctl enable bluetooth.service
 	sudo systemctl enable cups.service
 	sudo systemctl enable docker.service
