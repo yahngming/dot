7054a08d2b03118ab419dacede98782b5d9a33bf
Yang Ming <yahngming@gmail.com>
2026-07-05 20:14:48 +0800

    Update user-overrides.js

--- .config/firefox/user-overrides.js
+++ .config/firefox/user-overrides.js
@@ -1,5 +1,6 @@
 user_pref("browser.startup.page", 3);
 user_pref("browser.urlbar.trimURLs", false);
+user_pref("network.http.http3.enable", false);
 user_pref("privacy.resistFingerprinting.letterboxing", false);
 user_pref("sidebar.verticalTabs", true);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
