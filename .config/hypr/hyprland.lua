e4249ea43ef0b5b77a6c7e0bc38587eacdcc03af
Yang Ming <yahngming@gmail.com>
2026-07-19 03:15:46 +0800

    Update hyprland.lua

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -199,8 +199,8 @@ hl.window_rule({
 	},
 	float = true,
 	pin = true,
-	size = { "monitor_w * 0.30", "monitor_h * 0.25" },
-	move = { "monitor_w * 0.70 - 15", "monitor_h * 0.75 - 15" },
+	size = { "monitor_w * 0.5", "monitor_h * 0.3" },
+	move = { "monitor_w * 0.5 - 20", "monitor_h * 0.7 - 20" },
 })
 
 hl.window_rule({
