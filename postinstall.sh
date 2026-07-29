d60f726ce4c61125ad176161ecd562a8c98fc704
Yang Ming <yahngming@gmail.com>
2026-07-30 01:42:00 +0800

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -5,15 +5,33 @@ arch() {
 	sudo sed -i '/#\[multilib\]/,+1s/^#//' /etc/pacman.conf
 	sudo pacman -Syu --needed --noconfirm archlinux-keyring
 	sudo pacman -S --noconfirm bluez lib32-mesa lib32-vulkan-icd-loader mesa pipewire pipewire-alsa pipewire-jack pipewire-pulse vulkan-icd-loader # driver
+	sudo pacman -S --noconfirm noto-fonts-cjk noto-fonts-emoji ttf-liberation ttf-noto-nerd ttf-terminus-nerd # font
 	sudo pacman -S --noconfirm lib32-vulkan-radeon ollama-rocm rocm-hip-runtime vulkan-radeon # amd
 	sudo pacman -S --noconfirm intel-media-driver lib32-vulkan-intel libvpl vpl-gpu-rt vulkan-intel # intel
-	sudo pacman -S --noconfirm 7zip aerc atool base-devel bat bc bluetui bluez-utils brightnessctl btop chafa cmus cups dante docker docker-compose evtest eza f3d fastfetch fd ffmpegthumbnailer fontforge fortune-mod fuse fzf git git-delta glow imagemagick impala jq keyd lazydocker less lf libnotify lolcat man-db matugen mpv ollama openssh pass perl-image-exiftool polkit poppler python-mutagen rclone ripgrep smbclient translate-shell udiskie unrar unzip w3m wiremix wireplumber yt-dlp zathura-pdf-poppler zoxide # base
-	sudo pacman -S --noconfirm noto-fonts-cjk noto-fonts-emoji ttf-liberation ttf-noto-nerd ttf-terminus-nerd # font
-	sudo pacman -S --noconfirm firefox gamescope gvim keepassxc libreoffice-fresh steam # gui
-	sudo pacman -S --noconfirm fcitx5-chinese-addons fcitx5-im fcitx5-mozc # ime
-	sudo pacman -S --noconfirm tesseract tesseract-data-eng tesseract-data-jpn tesseract-data-kor #ocr
 	sudo pacman -S --noconfirm awww cliphist foot fuzzel grim hypridle hyprland hyprlock kanshi mako niri qt6-wayland satty slurp swaybg waybar wl-clipboard wl-mirror wlr-randr xwayland-satellite # wayland
 	sudo pacman -S --noconfirm clipmenu dunst maim nsxiv numlockx unclutter xclip xcompmgr xdg-desktop-portal-gtk xdo xfce4 xorg-server xorg-xinit xorg-xinput xorg-xrandr xscreensaver xwallpaper # xorg
+	sudo pacman -S --noconfirm 7zip
+	sudo pacman -S --noconfirm aerc atool
+	sudo pacman -S --noconfirm base-devel bat bc bluetui bluez-utils brightnessctl btop
+	sudo pacman -S --noconfirm chafa cmus cups
+	sudo pacman -S --noconfirm dante docker docker-compose
+	sudo pacman -S --noconfirm evtest eza
+	sudo pacman -S --noconfirm f3d fastfetch fcitx5-chinese-addons fcitx5-im fcitx5-mozc fd ffmpegthumbnailer firefox fontforge fortune-mod fuse fzf
+	sudo pacman -S --noconfirm gamescope git git-delta glow gvim
+	sudo pacman -S --noconfirm imagemagick impala
+	sudo pacman -S --noconfirm jq
+	sudo pacman -S --noconfirm keepassxc keyd
+	sudo pacman -S --noconfirm lazydocker less lf libnotify libreoffice-fresh lolcat
+	sudo pacman -S --noconfirm man-db matugen mpv
+	sudo pacman -S --noconfirm ollama openssh
+	sudo pacman -S --noconfirm pass perl-image-exiftool polkit poppler python-mutagen
+	sudo pacman -S --noconfirm rclone ripgrep
+	sudo pacman -S --noconfirm smbclient steam
+	sudo pacman -S --noconfirm tesseract tesseract-data-eng tesseract-data-jpn tesseract-data-kor translate-shell
+	sudo pacman -S --noconfirm udiskie unrar unzip
+	sudo pacman -S --noconfirm w3m wiremix wireplumber
+	sudo pacman -S --noconfirm yt-dlp
+	sudo pacman -S --noconfirm zathura-pdf-poppler zoxide
 	sudo systemctl enable bluetooth.service
 	sudo systemctl enable cups.service
 	sudo systemctl enable docker.service
